e75623d9cbff429a94fc3ac540f8f321060027e0
Amey Sakhadeo <me@ameyms.com>
2013-08-19 21:35:57 +0530

    Reverting license text to MIT license

--- jqterm.js
+++ jqterm.js
@@ -1,7 +1,6 @@
 /**
 * jqterm.js v0.2.0 by @ameyms
-* Copyright 2013 Amey Sakhadeo.
-* http://www.apache.org/licenses/LICENSE-2.0
+* Copyright (c) 2013 Amey Sakhadeo http://ameyms.com
 */
 
 if (!jQuery) { throw new Error("JqTerm requires jQuery") }
@@ -11,18 +10,8 @@ if (!jQuery) { throw new Error("JqTerm requires jQuery") }
  * http://ameyms.github.com/jqterm
  * ========================================================================
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ======================================================================== */
+ * Licensed under the MIT license.
+ */
 
 
 (function($, window)
